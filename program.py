5c88f46dcdf1823a3240ba4c3619585c58e3232f
cssrumi <33814533+cssrumi@users.noreply.github.com>
2018-10-05 16:23:52 +0200

    Update program.py

--- program.py
+++ program.py
@@ -15,16 +15,20 @@ def main():
     s2.push('dwa')
     s3 = s1 + s2
 
-    try:
-        s3 += 'adam'
-    except ValueError as e:
-        print('Error: ', e)
+    # try:
+    #     s3 += 'adam'
+    # except ValueError as e:
+    #     print('Error: ', e)
 
     print('size of stack1 =', s1.size())
     print('size of stack2 =', s2.size())
     print('size of stack3 =', s3.size())
 
-    print(type(None))
+    [print(obj) for obj in s3]
+    
+    s4 = Stack()
+    for obj in s4:
+        print(obj)
 
     # print('size of stack =', s1.size())
     # print(s1.pop())
